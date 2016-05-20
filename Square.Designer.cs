6468b28fe2411cbd886d38921775d77a89d2060f
Effy Wu <effywu1993@gmail.com>
2016-05-20 16:20:07 -0400

    Add files via upload

---
+++ Square.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Minesweeper
+{
+    partial class Square
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn.Location = new System.Drawing.Point(0, 0);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(24, 24);
+            this.btn.TabIndex = 1;
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Square
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.label1);
+            this.Name = "Square";
+            this.Size = new System.Drawing.Size(24, 24);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Square : UserControl
+    {
+        Main parent;
+        public Square(Main parent)
+        {
+            InitializeComponent();
+            this.parent = parent;
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+ControlStyles.UserPaint |
+ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.DoubleBuffered = true;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            //btn.Visible = false;
+        }
+
+        public Label getLbl
+        {
+            get { return label1; }
+        }
+
+        public string setLbl
+        {
+            set { label1.Text = value; }
+        }
+
+        public Button getBtn 
+        {
+            get { return btn; }
+
+        }
+    
+
+    }
+}
